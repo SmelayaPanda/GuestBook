9c89f1e088f00f1d217b130b64bd91bbc21371f4
SmelayaPanda <smelayapanda@mail.ru>
2016-06-09 13:46:07 +0600

    Initial commit

---
+++ src/MyServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Smela on 09.06.2016.
+ */
+@WebServlet("/")
+public class MyServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setAttribute("name",request.getRequestURI().replace("/"," "));
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+
+    }
+}
